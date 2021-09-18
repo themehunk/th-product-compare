1054f65644b615ff5c4c6b44d2977bb57fd419d1
upcominginfo <rahulvarma722@gmail.com>
2021-09-18 15:31:25 +0530

    table making product

--- assets/fscript.js
+++ assets/fscript.js
@@ -3,50 +3,50 @@
     init: function () {
       thCompare.bind();
     },
-    getPrevId: function (cookieNAme) {
-      const getCookie = document.cookie;
-      const splitted = getCookie.split(";");
-      const getThCompare = splitted.find((getStr) =>
-        getStr.includes(cookieNAme)
-      );
-      if (getThCompare) {
-        const getPRoductId = getThCompare.split("=");
-        if (getPRoductId.length > 1 && getPRoductId[1]) {
-          return getPRoductId[1].split(",");
-        }
-      }
-    },
-    setId: function (id, openPopup = false) {
-      const date = new Date();
-      date.setDate(date.getDate() + 1);
-      const expires = date.toUTCString();
-      const cookieNAme = "th_compare_product";
-      let previousCookie = thCompare.getPrevId(cookieNAme);
-      let cookieValue = id;
-      let updateCookie = true;
-      if (previousCookie) {
-        let getExist = previousCookie.find((id_) => id_ == id);
-        if (getExist) {
-          cookieValue = previousCookie.join(",");
-          updateCookie = false;
-        } else {
-          cookieValue = previousCookie.join(",") + "," + id;
-        }
-      }
-      //   update cookies
-      if (updateCookie) {
-        document.cookie =
-          cookieNAme + "=" + cookieValue + "; expires=" + expires + ";";
-      }
-      //   open popup
-      thCompare.openAndaddPopup(cookieValue);
-    },
+    // getPrevId: function (cookieNAme) {
+    //   const getCookie = document.cookie;
+    //   const splitted = getCookie.split(";");
+    //   const getThCompare = splitted.find((getStr) =>
+    //     getStr.includes(cookieNAme)
+    //   );
+    //   if (getThCompare) {
+    //     const getPRoductId = getThCompare.split("=");
+    //     if (getPRoductId.length > 1 && getPRoductId[1]) {
+    //       return getPRoductId[1].split(",");
+    //     }
+    //   }
+    // },
+    // setId: function (id, openPopup = false) {
+    //   const date = new Date();
+    //   date.setDate(date.getDate() + 1);
+    //   const expires = date.toUTCString();
+    //   const cookieNAme = "th_compare_product";
+    //   let previousCookie = thCompare.getPrevId(cookieNAme);
+    //   let cookieValue = id;
+    //   let updateCookie = true;
+    //   if (previousCookie) {
+    //     let getExist = previousCookie.find((id_) => id_ == id);
+    //     if (getExist) {
+    //       cookieValue = previousCookie.join(",");
+    //       updateCookie = false;
+    //     } else {
+    //       cookieValue = previousCookie.join(",") + "," + id;
+    //     }
+    //   }
+    //   //   update cookies
+    //   if (updateCookie) {
+    //     document.cookie =
+    //       cookieNAme + "=" + cookieValue + "; expires=" + expires + ";";
+    //   }
+    //   //   open popup
+    //   thCompare.openAndaddPopup(cookieValue);
+    // },
     addProduct: function (e) {
       e.preventDefault();
       let thisBtn = $(this);
       let getProductId = thisBtn.attr("data-th-product-id");
       if (parseInt(getProductId)) {
-        thCompare.setId(getProductId);
+        thCompare.openAndaddPopup(getProductId);
       }
     },
     openAndaddPopup: function (ids = "") {
@@ -104,9 +104,9 @@
   /* <div class="th-compare-output-wrap" id="th-compare-output-wrap"><div class="th-compare-output-wrap-inner"><div class="th-compare-output-close"><span class="dashicons dashicons-no-alt"></span></div><div class="th-compare-output-product"></div></div></div> */
 }
 
-console.log("current cookiew", document.cookie);
+// console.log("current cookiew", document.cookie);
 
-// var delete_cookie = function(name) {
-//     document.cookie = name + '=;expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+// var delete_cookie = function (name) {
+//   document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:01 GMT;";
 // };
-// delete_cookie('th_compare_product');
+// delete_cookie("th_compare_product");
