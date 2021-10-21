9cc99968f3d212e75368ab62ad860867714dc90b
upcominginfo <rahulvarma722@gmail.com>
2021-10-21 16:40:55 +0530

    style removed

--- admin/inc.php
+++ admin/inc.php
@@ -11,6 +11,7 @@ class th_product_compare
         add_action('admin_menu', array($this, 'admin_menu'));
         add_action('admin_enqueue_scripts', array($this, 'enqueue_admin_script'));
         add_action('wp_enqueue_scripts', array($this, 'enqueue_front_script'));
+        add_filter('plugin_action_links_' . plugin_basename(TH_PRODUCT_PATH . '/' . basename(TH_PRODUCT_BASE_NAME)), array($this, 'add_menu_links'));
         $this->localizeOption = get_option('th_compare_option');
     }
     public static function get()
@@ -38,7 +39,13 @@ class th_product_compare
     }
     public function admin_menu()
     {
-        add_menu_page(__('Th Product Compare', 'th-product-compare'), __('Th Product Compare', 'th-product-compare'), 'th_product_compare_manager', 'th-product-compare', array($this, 'display_addons'), TH_PRODUCT_URL.'/assets/img/th-nav-logo.png');
+        add_menu_page(__('Th Product Compare', 'th-product-compare'), __('Th Product Compare', 'th-product-compare'), 'th_product_compare_manager', 'th-product-compare', array($this, 'display_addons'), TH_PRODUCT_URL . '/assets/img/th-nav-logo.png');
+    }
+    public function add_menu_links($links)
+    {
+        $links[] = '<a href="' . admin_url("admin.php?page=th-product-compare") . '">' . __('Settings', 'th-compare-product') . '</a>';
+        $links['premium'] = '<a href="#"><b>' . __('Get Pro', 'th-compare-product') . '</b></a>';
+        return $links;
     }
     public function display_addons()
     {
@@ -51,53 +58,20 @@ class th_product_compare
             include_once "page.php";
         }
     }
-
-    public function thCompareAddonStyle()
-    {
-        $th_compare_option = $this->localizeOption;
-        $styleCSs = '';
-        if (!empty($th_compare_option)) {
-            foreach ($th_compare_option as $ckey => $cvalue) {
-                if ($ckey == 'fore-ground-bgth-idbackground-color') {
-                    $styleCSs .= '.th-compare-popup-dummy{background-color:' . $cvalue . ';}';
-                } else if ($ckey == 'dummy-border-colorth-idborder-color') {
-                    $styleCSs .= '.th-compare-popup-dummy .inner-wrap_{border-color:' . $cvalue . ';}';
-                } else if ($ckey == 'heading-styleth-idbackground-color') {
-                    $styleCSs .= '.th-compare-popup-dummy .inner-wrap_ .heading{background-color:' . $cvalue . ';}';
-                } else if ($ckey == 'heading-styleth-idcolor') {
-                    $styleCSs .= '.th-compare-popup-dummy .inner-wrap_ .heading{color:' . $cvalue . ';}';
-                } else if ($ckey == 'row-odd-bgth-idbackground-color') {
-                    $styleCSs .= '.th-compare-popup-dummy .inner-wrap_ .table_ table tr:nth-child(odd){background-color:' . $cvalue . ';}';
-                } else if ($ckey == 'row-even-bgth-idbackground-color') {
-                    $styleCSs .= '.th-compare-popup-dummy .inner-wrap_ .table_ table tr:nth-child(even){background-color:' . $cvalue . ';}';
-                } else if ($ckey == 'rating-colorth-idcolor') {
-                    $styleCSs .= '.th-compare-popup-dummy .inner-wrap_ .table_ .rating_{color:' . $cvalue . ';}';
-                } else if ($ckey == 'remove-btn-colorth-idcolor') {
-                    $styleCSs .= '.th-compare-popup-dummy .inner-wrap_ .table_ .rm-product{color:' . $cvalue . ';}';
-                } else if ($ckey == 'close-btn-styleth-idbackground-color') {
-                    $styleCSs .= '.th-compare-popup-dummy .inner-wrap_ .close-btn_{background-color:' . $cvalue . ';}';
-                } else if ($ckey == 'close-btn-styleth-idcolor') {
-                    $styleCSs .= '.th-compare-popup-dummy .inner-wrap_ .close-btn_{color:' . $cvalue . ';}';
-                }
-            }
-        }
-        return $styleCSs ? $styleCSs : '';
-    }
-
     public function enqueue_admin_script($hook)
     {
-        // if ('toplevel_page_business-popup' != $hook) return;
+        // if ('check-plugin' != $hook) return;
         wp_enqueue_style('th-color-picker', TH_PRODUCT_URL . 'assets/color/nano.min.css', false);
         wp_enqueue_style('th-product-compare-style', TH_PRODUCT_URL . 'assets/style.css', false);
         wp_enqueue_script('th-color-picker', TH_PRODUCT_URL . 'assets/color/pickr.es5.min.js', array('jquery'), 1, true);
         wp_enqueue_script('th-product-js', TH_PRODUCT_URL . 'assets/js/script.js', [], 1, true);
-        wp_localize_script('th-product-js', 'th_product', array('th_product_ajax_url' => admin_url('admin-ajax.php'), 'th_compare_style_local' => $this->thCompareAddonStyle()));
+        wp_localize_script('th-product-js', 'th_product', array('th_product_ajax_url' => admin_url('admin-ajax.php')));
     }
 
     public function enqueue_front_script()
     {
         wp_enqueue_style('th-product-compare-style-front', TH_PRODUCT_URL . 'assets/fstyle.css', false);
         wp_enqueue_script('th-product-js', TH_PRODUCT_URL . 'assets/js/fscript.js', array('jquery'), 1, true);
-        wp_localize_script('th-product-js', 'th_product', array('th_product_ajax_url' => admin_url('admin-ajax.php'), 'th_compare_style_local' => $this->localizeOption));
+        wp_localize_script('th-product-js', 'th_product', array('th_product_ajax_url' => admin_url('admin-ajax.php')));
     }
 }
--- th-product-compare.php
+++ th-product-compare.php
@@ -8,10 +8,10 @@
 * Text Domain: th-product-compare
  */
 if (!defined('ABSPATH')) exit;
-
-define('TH_PRODUCT_URL', plugin_dir_url(__FILE__));
-define('TH_PRODUCT_PATH', plugin_dir_path(__FILE__));
-if (!function_exists('th_product_compare_loaded')) {
+if (!function_exists('th_product_compare_loaded_pro')) {
+    define('TH_PRODUCT_URL', plugin_dir_url(__FILE__));
+    define('TH_PRODUCT_PATH', plugin_dir_path(__FILE__));
+    define('TH_PRODUCT_BASE_NAME', __FILE__);
     include_once(TH_PRODUCT_PATH . 'admin/inc.php');
     include_once(TH_PRODUCT_PATH . 'admin/front/front.php');
     include_once(TH_PRODUCT_PATH . 'admin/front/product.php');
